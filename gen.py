b6ce288ad08fb5676acd43398e8bb18cd4b6eee7
Renan Samuel da Silva <uber.renan@gmail.com>
2015-10-02 10:56:16 -0300

    better identation

--- gen.py
+++ gen.py
@@ -5,19 +5,26 @@ import sys
 M = int(sys.argv[1])
 N = int(sys.argv[2])
 
-a = [[ random.randint(1,25) for i in range(0, N) ] for j in range(0, M)]
-b = [ random.randint(1,25) for i in range(0, M) ]
-c = [ random.randint(1,25) for i in range(0, N) ]
+a = [[ random.randint( 1,25) for i in range(0, N) ] for j in range(0, M)]
+b = [  random.randint(200,250) for i in range(0, M) ]
+c = [  random.randint( 1,25) for i in range(0, N) ]
+
+
 
 for i in b:
     print(i, end=" ")
 
 print("")
 
+
+
 for i in c:
     print(i, end=" ")
 
 print("")
+
+
+
 for i in a:
     for j in i:
         print(j, end=" ")
