18f0d18b0fe411d4279a348b26e08f329ddaf0a9
Renan Samuel da Silva <uber.renan@gmail.com>
2015-10-09 08:48:50 -0300

    Better gen

--- gen.py
+++ gen.py
@@ -4,10 +4,11 @@ import sys
 
 M = int(sys.argv[1])
 N = int(sys.argv[2])
+S = int(sys.argv[3])
 
-a = [[ random.randint( 1,25) for i in range(0, N) ] for j in range(0, M)]
-b = [  random.randint(200,250) for i in range(0, M) ]
-c = [  random.randint( 1,25) for i in range(0, N) ]
+a = [[ random.randint(            1,         25 ) for i in range(0, N) ] for j in range(0, M)]
+b = [  random.randint( int(N*0.8*S),int(N*1.4*S)) for i in range(0, M) ]
+c = [  random.randint(            1,         25 ) for i in range(0, N) ]
 
 
 
