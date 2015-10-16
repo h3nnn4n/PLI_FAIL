563c111539c45fe65e7e892bb0f796e6485cc808
Renan Samuel da Silva <uber.renan@gmail.com>
2015-10-16 10:23:22 -0300

    List related files

---
+++ list.h
@@ -0,0 +1,19 @@
+#ifndef __list_pli
+#define __list_pli
+
+#include "matrix.h"
+
+typedef struct __instance _instance;
+
+typedef struct __list{
+    struct __list *next;
+    _instance     *ins;
+} _list;
+
+
+_list*       list_init();
+_instance*   list_pop(_list *);
+int          list_size(_list *);
+void         list_insert(_list *, _instance*);
+
+#endif // __list_pli
